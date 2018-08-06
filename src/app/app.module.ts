8c3b5eb39d1509af7c9d60eceae68072025a6a46
shadid12 <shadidhaque2012@gmail.com>
2018-08-06 02:32:48 -0400

    abcd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './containers/signup/signup.component';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {  MatToolbarModule, 
+          MatButtonModule, 
+          MatSidenavModule, 
+          MatIconModule, 
+          MatListModule, 
+          MatCardModule 
+        } from '@angular/material';
+  
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/containers/signup/signup.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  
+  email :FormControl  = new FormControl('', [Validators.required, Validators.email]);
+  password: FormControl = new FormControl('', [Validators.required]);
+  hide  :boolean      = true;
+
+  constructor() { }
+
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'You must enter a value' :
+        this.email.hasError('email') ? 'Not a valid email' :
+            '';
+  }
+
+  onSubmt(form: any): void { 
+    console.log(`Email: ${this.email.value} Pass: ${this.password.value}`);
+
+  }
+
+  ngOnInit() {
+  }
+
+}
